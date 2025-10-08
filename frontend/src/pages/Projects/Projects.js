45e769961cc66b0e9eef6c81353fad211b735517
Ashish1592025 <gargashish734@gmail.com>
2025-10-08 19:13:46 +0530

    commit
    mern

---
+++ frontend/src/pages/Projects/Projects.js
@@ -0,0 +1,87 @@
+import React from "react";
+import "./Projects.css";
+// import Spin from "react-reveal/Spin";
+import WeatherApp from '../../assets/images/WeatherApp.png';
+import ShoppingCart from '../../assets/images/ShoppingCart.png';
+
+
+const Projects = () => {
+  return (
+    <>
+      <div className="container project" id="project">
+        <h2 className="col-12 mt-3 mb-1 text-center text-uppercase">
+          TOP RECENT PROJECTS
+        </h2>
+        <hr />
+        <p className="pb-3 text-center">
+          {/* add here caption which description is related to all project */}
+          Practical full-stack applications developed using MERN technologies, focusing on clean code, reusability, and optimized user experience.
+        </p>
+        {/* card design */}
+        
+        <div className="row" id="ads">
+          {/* <Spin> */}
+          <div className="col-md-4">
+            <div className="card rounded">
+              <div className="card-image">
+                <span className="card-notify-badge">Mern Stack</span>
+                <img src="https://innois.in/wp-content/uploads/2024/06/showing-cart-trolley-shopping-online-sign-graphic-scaled.jpg" alt="project1" />
+              </div>
+              <div className="card-image-overly m-auto mt-3">
+                <span className="card-detail-badge">React JS</span>
+                <span className="card-detail-badge">Node JS</span>
+                <span className="card-detail-badge">Express JS</span>
+                <span className="card-detail-badge">MongoDB</span>
+              </div>
+              <div className="card-body text-center">
+                <div className="ad-title m-auto">
+                  <h5 className="text-uppercase">Book My Show App</h5>
+                </div>
+                <a className="ad-btn" href="https://book-my-show-dw4g.vercel.app/" target="_blank">View</a>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-4">
+            <div className="card rounded">
+              <div className="card-image">
+                <span className="card-notify-badge">React Website</span>
+                <img src={WeatherApp} alt="project1" />
+              </div>
+              <div className="card-image-overly m-auto mt-3">
+                <span className="card-detail-badge">React JS</span>
+                <span className="card-detail-badge">CSS</span>
+              </div>
+              <div className="card-body text-center">
+                <div className="ad-title m-auto">
+                  <h5 className="text-uppercase">Weather App</h5>
+                </div>
+                <a className="ad-btn" href="https://weather-app-gold-pi-42.vercel.app/" target="_blank">View</a>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-4">
+            <div className="card rounded">
+              <div className="card-image">
+                <span className="card-notify-badge">React Website</span>
+                <img src={ShoppingCart} alt="project2" />
+              </div>
+              <div className="card-image-overly m-auto mt-3">
+                <span className="card-detail-badge">React JS</span>
+                <span className="card-detail-badge">CSS</span>
+              </div>
+              <div className="card-body text-center">
+                <div className="ad-title m-auto">
+                  <h5 className="text-uppercase">Shopping Cart</h5>
+                </div>
+                <a className="ad-btn" href="https://shopping-cart-vert-eight.vercel.app/" target="_blank">View</a>
+              </div>
+            </div>
+          </div>
+          {/* </Spin> */}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Projects;
